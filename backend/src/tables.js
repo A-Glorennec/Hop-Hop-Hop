37ed5b69b93be560b6a4430d8f8c18ad5818050f
ANAIS GLORENNEC <zans_69@hotmail.fr>
2024-06-25 16:17:33 +0200

    first commit

---
+++ backend/src/tables.js
@@ -0,0 +1,53 @@
+/* ************************************************************************* */
+// Register Data Managers for Tables
+/* ************************************************************************* */
+
+// Import the manager modules responsible for handling data operations on the tables
+const UserManager = require("./models/UserManager");
+const GroupManager = require("./models/GroupManager");
+const TransactionManager = require("./models/TransactionManager");
+const EventManager = require("./models/EventManager");
+const RemindEventManager = require("./models/RemindEventManager");
+const TaskManager = require("./models/TaskManager");
+const DocumentManager = require("./models/DocumentManager");
+const RecipeManager = require("./models/RecipeManager");
+const ContactManager = require("./models/ContactManager");
+
+const managers = [
+  UserManager,
+  GroupManager,
+  TransactionManager,
+  EventManager,
+  RemindEventManager,
+  TaskManager,
+  DocumentManager,
+  RecipeManager,
+  ContactManager,
+];
+
+// Create an empty object to hold data managers for different tables
+const tables = {};
+
+// Register each manager as data access point for its table
+managers.forEach((ManagerClass) => {
+  const manager = new ManagerClass();
+
+  tables[manager.table] = manager;
+});
+
+/* ************************************************************************* */
+
+// Use a Proxy to customize error messages when trying to access a non-existing table
+
+// Export the Proxy instance with custom error handling
+module.exports = new Proxy(tables, {
+  get(obj, prop) {
+    // Check if the property (table) exists in the tables object
+    if (prop in obj) return obj[prop];
+
+    // If the property (table) does not exist, throw a ReferenceError with a custom error message
+    throw new ReferenceError(
+      `tables.${prop} is not defined. Did you register it in ${__filename}?`
+    );
+  },
+});
